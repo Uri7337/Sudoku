4a9654dca9f26356d66e18d9e0ba8da37942a907
uri <uri@uri.cz>
2025-08-22 13:58:01 +0200

    edit

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-let sudokuGrid = document.getElementById("sudoku-container");
 
+
+const fruits = ["1", "2", "3", "4"];
+
+let text = "";
+let sudokuGrid = document.getElementById("sudoku-container");
+fruits.forEach(myFunction);
+//text += "</ul>";
+
+sudokuGrid.innerHTML = text;
+
+function myFunction(value) {
+  text += "<div>" + value + "</div>";
+}
