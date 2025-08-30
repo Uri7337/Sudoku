b0110727f7bf69beba75bf25069b47babf8efbf1
uri <uri@uri.cz>
2025-08-30 20:16:10 +0200

    feat(script) final -> generated <- more accurate ; added contenteditable on fields and restrictions to numbers 0-9 ; bob variable for testing

--- script.js
+++ script.js
@@ -2,21 +2,40 @@
 
 //Sudoku fields
 let fields = new Array(9);
-let finalGrid = "";
+let generatedGrid = "";
 let sudokuGridContainer = document.getElementById("sudoku-container");
+// field valid values:
+const fieldVal1 = "&nbsp;";
+const fieldVal2 = "0"; 
 
-function addToFinalGrid(value) {
-  finalGrid += "<div class=\"field\">" + value + "</div>";
+function addToGeneratedGrid(value) {
+  generatedGrid += "<div contenteditable=\"true\" class=\"field\">" + value + "</div>";
 }
 
 //fill fields with ones
 for (let i = 0; i < 9; i++) {
     fields[i] = new Array(9);
     fields[i].fill(1);
-    fields[i].forEach(addToFinalGrid);
+    fields[i].forEach(addToGeneratedGrid);
 }
 
-//console.log(fields); // 
 
-sudokuGridContainer.innerHTML = finalGrid;
 
+sudokuGridContainer.innerHTML = generatedGrid;
+
+const fieldsContent = document.getElementsByClassName("field");
+
+for (let field of fieldsContent) {
+  field.addEventListener("input", () => {
+    field.innerText = field.innerText.replace(/[^0-9]/g, "");
+
+    if (field.innerText.length > 1) {
+      field.innerText = field.innerText.slice(0, 1);
+    }else if(field.innerText.length === 0){
+      field.innerHTML = fieldVal1;
+    }
+
+  });
+}
+
+let bob = document.getElementsByClassName("field");
